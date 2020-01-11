2d0934f24baa09481a57792fd2bfc639a50b57b2
chengweiyishuguang <984425247@qq.com>
2020-01-11 20:44:16 +0800

    首页文章列表-样式调整

--- src/views/tab-bar/index.vue
+++ src/views/tab-bar/index.vue
@@ -3,14 +3,14 @@
       <!-- 子路由出口 -->
        <router-view />
       <!-- tab-bar标签栏 -->
-       <!-- <van-tabbar route>
+       <van-tabbar route>
       <van-tabbar-item icon="wap-home-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item icon="comment-o" to="/qa">问答</van-tabbar-item>
       <van-tabbar-item icon="video-o" to="/video">视频</van-tabbar-item>
       <van-tabbar-item
       icon="user-o" to="/my"
       >{{$store.state.user?'我的':'未登录'}}</van-tabbar-item>
-    </van-tabbar> -->
+    </van-tabbar>
   </div>
 </template>
 
