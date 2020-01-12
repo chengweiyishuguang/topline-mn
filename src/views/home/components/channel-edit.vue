f29d5976e1e1f65c44713a9149bc2ddcae27d18a
chengweiyishuguang <984425247@qq.com>
2020-01-12 09:44:11 +0800

    频道编辑-展示剩余频道

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -15,15 +15,17 @@
 
    <van-grid square>
   <van-grid-item
-    v-for="value in 8"
-    :key="value"
-    text="文字"
+    v-for="channel in remainingChannels"
+    :key="channel.id"
+    :text="channel.name"
   />
 </van-grid>
   </div>
 </template>
 
 <script>
+import { getAllChannels } from '@/api/channel'
+
 export default {
   name: 'ChannelEdit',
   props: {
@@ -31,6 +33,35 @@ export default {
       type: Array,
       required: true
     }
+  },
+  data () {
+    return {
+      allChannels: []// 所有频道
+    }
+  },
+  computed: {
+    remainingChannels () {
+      const { allChannels, userChannels } = this
+      // 剩余频道 = 所有频道 - 我的频道
+      const channels = []
+      // 遍历所有频道
+      allChannels.forEach(item => {
+        // 如果我的频道中不包含被遍历的频道，则要添加到推荐频道
+        if (!userChannels.find(c => c.id === item.id)) {
+          channels.push(item)
+        }
+      })
+      return channels
+    }
+  },
+  methods: {
+    async loadAllChannels () {
+      const { data } = await getAllChannels()
+      this.allChannels = data.data.channels
+    }
+  },
+  created () {
+    this.loadAllChannels()
   }
 
 }
