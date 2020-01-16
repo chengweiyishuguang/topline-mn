f41c60de33fbdb15bb571d28a18c2090024ab4fa
chengweiyishuguang <984425247@qq.com>
2020-01-16 10:38:47 +0800

    Token处理-解析JWT获取用户id

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { getItem, setItem } from '@/untils/storage.js'
+// 解析token的包
+import decodeJWT from 'jwt-decode'
 
 Vue.use(Vuex)
 
@@ -10,6 +12,10 @@ export default new Vuex.Store({
   },
   mutations: {
     setUser (state, data) {
+      if (data && data.token) {
+        // 解析data.token
+        data.id = decodeJWT(data.token).user_id
+      }
       state.user = data
       // 为了防止刷新数据丢失，这里还需要把数据放到本地储存持久化
       setItem('user', state.user)
@@ -18,5 +24,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+
   }
 })
