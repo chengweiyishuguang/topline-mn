44593bd346e0f923554c9bbe05b795187e156b4b
chengweiyishuguang <984425247@qq.com>
2020-01-17 13:55:44 +0800

    评论回复-发布评论-组件封装

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -32,16 +32,46 @@
         />
     </van-list>
     <!-- /评论的回复列表 -->
+    <!-- 底部区域 -->
+    <div class="footer">
+      <van-button
+      class="write-btn"
+      type="default"
+      round
+      size="small"
+      @click="isPostShow=true"
+      >写评论</van-button>
+      <van-icon
+      color="#e5645f"
+      name="good-job"
+      />
+
+    </div>
+    <!-- /底部区域 -->
+    <!-- 发布回复 -->
+    <van-popup
+      v-model="isPostShow"
+      position="bottom"
+    >
+  <post-comment
+    v-model="PostMessage"
+  @click-post="onPost"
+
+  />
+    </van-popup>
+       <!--/ 发布回复 -->
   </div>
 </template>
 
 <script>
 import CommentItem from './comment-item'
 import { getComments } from '@/api/comment'
+import PostComment from './post-comment'
 export default {
   name: 'commentReply',
   components: {
-    CommentItem
+    CommentItem,
+    PostComment
   },
   props: {
     comment: {
@@ -55,7 +85,9 @@ export default {
       loading: false,
       finished: false,
       offset: null,
-      limit: 20
+      limit: 20,
+      isPostShow: false, // 发布回复的显示状态
+      PostMessage: ''
     }
   },
   methods: {
@@ -99,12 +131,43 @@ export default {
       } else {
         this.finished = true
       }
+    },
+    async onPost () {
+      console.log('on post')
     }
   }
 
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.comment-reply {
+  padding-bottom:44px;
+}
+.reply-nav-bar {
+  background: #fff;
+  .van-icon, .van-nav-bar__title, .van-nav-bar__right {
+    color: #333;
+  }
+}
+.footer {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 44px;
+  border-top: 1px solid #d8d8d8;
+  background-color: #fff;
+  .write-btn {
+    width: 160px;
+  }
+  .van-icon {
+    font-size: 20px;
+  }
+}
 
 </style>
---
+++ src/views/article/components/post-comment.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="post-comment">
+    <van-field
+    v-model="message"
+          class="post-field"
+            rows="2"
+            autosize
+            :value= "value"
+            @input="$emit('input',$event)"
+            type="textarea"
+            maxlength="50"
+            placeholder="请输入留言"
+            show-word-limit
+          />
+          <van-button
+           size="small"
+           type="primary"
+           :disabled="!value"
+           @click="$emit('click-post')"
+
+           >发布</van-button>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PostComment',
+  // 声明接收comment-reply传值
+  props: {
+    value: {
+      type: String,
+      required: true
+
+    }
+  },
+  data () {
+    return {
+      message: ''
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="less">
+.post-comment {
+  display: flex;
+  align-items: flex-end;
+  padding:10px;
+  .post-field {
+    background-color: #f5f7f9;
+    margin-right:15px;
+  }
+}
+
+</style>
