752edb79e06178d4bb515187b897e6bcfa94f722
chengweiyishuguang <984425247@qq.com>
2020-01-22 15:39:02 +0800

    编辑用户资料-头像上传-完成

--- src/api/user.js
+++ src/api/user.js
@@ -87,3 +87,15 @@ export const updateUserProfile = data => {
 
   })
 }
+
+/**
+ *更新用户照片资料
+ */
+export const updateUserPhoto = data => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/photo',
+    data
+
+  })
+}
