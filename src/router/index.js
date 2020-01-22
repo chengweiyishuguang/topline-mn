a957c368f0959539147e8c5b9fc43d6699f4ea24
chengweiyishuguang <984425247@qq.com>
2020-01-22 19:14:10 +0800

    小智同学-准备

--- src/router/index.js
+++ src/router/index.js
@@ -57,6 +57,12 @@ const routes = [{
   name: 'user-profile',
   component: () => import('@/views/user-profile')
 
+},
+{
+  path: '/user/chat',
+  name: 'user-chat',
+  component: () => import('@/views/user-chat')
+
 }
 ]
 
