76c06ed2a20e1f6d23cf70e4343f4ca2c9c8f21c
chengweiyishuguang <984425247@qq.com>
2020-02-08 10:36:57 +0800

    我的收藏历史-布局

--- src/views/user-articles/index.vue
+++ src/views/user-articles/index.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="uesr-articles">
-      用户文章
+    <van-nav-bar title="我的收藏/历史/作品" left-arrow @click-left="$router.back()" />
+    <van-tabs v-model="active">
+      <van-tab title="我的收藏">内容 1</van-tab>
+      <van-tab title="我的历史">内容 2</van-tab>
+      <van-tab title="我的作品">
+          <User-ariticle/>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
+import UserAriticle from './components/article'
 export default {
-  name: 'UserAriticles'
+  name: 'UserAriticles',
+  components: {
+    UserAriticle
+  },
+  data () {
+    return {
+      active: 0// 控制标签激活的选项卡
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
